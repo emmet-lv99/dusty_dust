986d5c9ea94e8367878bd47acd206d295be63622
dachan <dachanisfull@gmail.com>
2024-01-17 18:06:18 +0900

    add hourly data

--- lib/component/cartegory_card.dart
+++ lib/component/cartegory_card.dart
@@ -1,6 +1,8 @@
+import 'package:dusty_dust/component/card_title.dart';
 import 'package:flutter/material.dart';
 
 import '../const/colors.dart';
+import 'main_card.dart';
 import 'main_stat.dart';
 
 class CartegoryCard extends StatelessWidget {
@@ -10,38 +12,12 @@ class CartegoryCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 160,
-      child: Card(
-        margin: EdgeInsets.symmetric(horizontal: 8.0),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(16.0),
-          ),
-        ),
-        color: lightColor,
+      child: MainCard(
         child: LayoutBuilder(builder: (context, constraints) {
           return Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: darkColor,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(16.0),
-                    topRight: Radius.circular(16.0),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Text(
-                    '종류별 통계',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
-              ),
+              CardTitle(title: '종류별 통계'),
               Expanded(
                 child: ListView(
                   scrollDirection: Axis.horizontal,
